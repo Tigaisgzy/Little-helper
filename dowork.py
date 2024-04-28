39158c6c1e52af1d5ce5957274f36e8d4d060e6c
甘梓阳 <3552971348@qq.com>
2024-04-28 17:02:42 +0800

    feat: 邮箱发送

--- dowork.py
+++ dowork.py
@@ -59,7 +59,7 @@ def init():
     session.headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
     }
-    return requests.Session()
+    return session
 
 
 def getCode(image):
