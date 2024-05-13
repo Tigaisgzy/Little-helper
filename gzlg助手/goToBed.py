dabe91bde9f891a1355e3ef0063b1f80d3866b32
甘梓阳 <3552971348@qq.com>
2024-05-13 23:13:43 +0800

    fix: 测试

--- gzlg助手/goToBed.py
+++ gzlg助手/goToBed.py
@@ -108,7 +108,8 @@ def doWork(session):
     data_hz = {
         'data': '{"SFFWN":"1","DDDM":"b2c1441606da4efbb9fe5b2b89226396","DDMC":"广州理工学院(博罗校区)","QDJD":114.08675193786623,"QDWD":23.186742693715477,"RWBH":"16FC8C91BCDDEC67E0630717000A97E1","QDPL":"2"}',
     }
-    if os.getenv('USERNAME')[:4] == '2023':
+    print(int(os.getenv('USERNAME')[:4]))
+    if int(os.getenv('USERNAME')[:4]) >= 2023:
         response = session.post(
             'https://xsfw.gzist.edu.cn/xsfw/sys/swmzncqapp/modules/studentCheckController/uniFormSignUp.do',
             cookies=cookies,
